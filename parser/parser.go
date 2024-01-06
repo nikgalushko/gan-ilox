aa302ba4babf91404dc0239275b125038592c5b2
Nikita Galushko <galnikrom@gmail.com>
2024-01-06 12:58:32 +0300

    parse error handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,11 +1,25 @@
 package parser
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/nikgalushko/gan-ilox/expr"
 	"github.com/nikgalushko/gan-ilox/token"
 )
 
 type Expr = expr.Expr[any]
+
+type PraseError []error
+
+func (e PraseError) Error() string {
+	var arr []string
+	for _, err := range e {
+		arr = append(arr, err.Error())
+	}
+
+	return strings.Join(arr, "\n")
+}
 
 type Parser struct {
 	tokens  []token.Token
@@ -16,93 +30,159 @@ func New(tokens []token.Token) *Parser {
 	return &Parser{tokens: tokens}
 }
 
-func (p *Parser) expression() Expr {
+func (p *Parser) Parse() (Expr, error) {
+	var pErr PraseError
+	for !p.isAtEnd() {
+		e, err := p.expression()
+		if err == nil || p.isAtEnd() {
+			return e, err
+		}
+
+		pErr = append(pErr, err)
+		p.synchronize()
+	}
+
+	return nil, pErr
+}
+
+func (p *Parser) synchronize() {
+	t := p.advance()
+
+	for !p.isAtEnd() {
+		if p.prev().Kind == token.Semicolon {
+			return
+		}
+
+		switch t.Kind {
+		case token.Var, token.For, token.While, token.If, token.Else, token.Return, token.Print, token.Fun, token.Class:
+			return
+		}
+
+		t = p.advance()
+	}
+}
+
+func (p *Parser) expression() (Expr, error) {
 	return p.equality()
 }
 
-func (p *Parser) equality() Expr {
-	e := p.comparison()
+func (p *Parser) equality() (Expr, error) {
+	e, err := p.comparison()
+	if err != nil {
+		return nil, err
+	}
 
 	for p.match(token.EqualEqual, token.BangEqual) {
 		operator := p.prev()
-		right := p.comparison()
+		right, err := p.comparison()
+		if err != nil {
+			return nil, err
+		}
 		e = expr.Binary[any]{Left: e, Operator: operator, Right: right}
 	}
 
-	return e
+	return e, nil
 }
 
-func (p *Parser) comparison() Expr {
-	e := p.term()
+func (p *Parser) comparison() (Expr, error) {
+	e, err := p.term()
+	if err != nil {
+		return nil, err
+	}
 
 	for p.match(token.Less, token.LessEqual, token.Greater, token.GreaterEqual) {
 		operator := p.prev()
-		right := p.term()
+		right, err := p.term()
+		if err != nil {
+			return nil, err
+		}
 		e = expr.Binary[any]{Left: e, Operator: operator, Right: right}
 	}
 
-	return e
+	return e, nil
 }
 
-func (p *Parser) term() Expr {
-	e := p.factor()
+func (p *Parser) term() (Expr, error) {
+	e, err := p.factor()
+	if err != nil {
+		return nil, err
+	}
 
 	for p.match(token.Plus, token.Minus) {
 		operator := p.prev()
-		right := p.factor()
+		right, err := p.factor()
+		if err != nil {
+			return nil, err
+		}
+
 		e = expr.Binary[any]{Left: e, Operator: operator, Right: right}
 	}
 
-	return e
+	return e, nil
 }
 
-func (p *Parser) factor() Expr {
-	e := p.unary()
+func (p *Parser) factor() (Expr, error) {
+	e, err := p.unary()
+	if err != nil {
+		return nil, err
+	}
 
 	for p.match(token.Slash, token.Star) {
 		operator := p.prev()
-		right := p.unary()
+		right, err := p.unary()
+		if err != nil {
+			return nil, err
+		}
+
 		e = expr.Binary[any]{Left: e, Operator: operator, Right: right}
 	}
 
-	return e
+	return e, nil
 }
 
-func (p *Parser) unary() Expr {
+func (p *Parser) unary() (Expr, error) {
 	if p.match(token.Bang, token.Minus) {
 		operator := p.prev()
-		right := p.unary()
-		return expr.Unary[any]{Operator: operator, Right: right}
+		right, err := p.unary()
+		if err != nil {
+			return nil, err
+		}
+
+		return expr.Unary[any]{Operator: operator, Right: right}, nil
 	}
 
 	return p.primary()
 }
 
-func (p *Parser) primary() Expr {
+func (p *Parser) primary() (Expr, error) {
 	if p.match(token.Number, token.String) {
-		return expr.Literal[any]{Value: p.prev().Literal}
+		return expr.Literal[any]{Value: p.prev().Literal}, nil
 	}
 
 	if p.match(token.True) {
-		return expr.Literal[any]{Value: true}
+		return expr.Literal[any]{Value: true}, nil
 	}
 	if p.match(token.False) {
-		return expr.Literal[any]{Value: false}
+		return expr.Literal[any]{Value: false}, nil
 	}
 	if p.match(token.Nil) {
-		return expr.Literal[any]{Value: nil}
+		return expr.Literal[any]{Value: nil}, nil
 	}
 
 	if p.match(token.LeftParen) {
-		e := p.expression()
+		e, err := p.expression()
+		if err != nil {
+			return nil, err
+		}
+
 		if !p.match(token.RightParen) {
 			panic("Expect ')' after expression")
 		}
 
-		return expr.Grouping[any]{Expression: e}
+		return expr.Grouping[any]{Expression: e}, nil
 	}
 
-	panic("unreachable code")
+	return nil, errors.New("Expect expression")
 }
 
 func (p *Parser) prev() token.Token {
