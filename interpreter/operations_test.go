a0231c602fafd600f8bfbc871fff22d4838472c9
Nikita Galushko <galnikrom@gmail.com>
2024-01-09 22:00:12 +0300

    rewrite arithmetic operations unit tests; cover numbers

--- interpreter/operations_test.go
+++ interpreter/operations_test.go
@@ -1,49 +1,64 @@
 package interpreter
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/nikgalushko/gan-ilox/token"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAdd_OK(t *testing.T) {
-	tests := map[string]struct {
-		a, b   token.Literal
-		expect token.Literal
-	}{
-		"int + int": {
-			a:      token.NewLiteralInt(123),
-			b:      token.NewLiteralInt(456),
-			expect: token.NewLiteralInt(579),
-		},
-		"int + float": {
-			a:      token.NewLiteralInt(4),
-			b:      token.NewLiteralFloat(5.0),
-			expect: token.NewLiteralFloat(9.0),
-		},
-		"float + int": {
-			a:      token.NewLiteralFloat(3.2),
-			b:      token.NewLiteralInt(5),
-			expect: token.NewLiteralFloat(8.2),
-		},
-		"string + string": {
-			a:      token.NewLiteralString("ab"),
-			b:      token.NewLiteralString("cd"),
-			expect: token.NewLiteralString("abcd"),
-		},
+func TestArithmeticNumbers(t *testing.T) {
+	operations := map[string]func(token.Literal, token.Literal) (token.Literal, error){
+		"+": add,
+		"-": sub,
+		"*": mul,
+		"/": div,
 	}
 
-	for title, args := range tests {
-		t.Run(title, func(t *testing.T) {
-			actual, err := add(args.a, args.b)
-			require.NoError(t, err)
-			require.Equal(t, args.expect, actual)
-		})
+	goldenInts := map[string]func(a, b int64) int64{
+		"+": func(a, b int64) int64 { return a + b },
+		"-": func(a, b int64) int64 { return a - b },
+		"*": func(a, b int64) int64 { return a * b },
+		"/": func(a, b int64) int64 { return a / b },
+	}
+	goldenFloats := map[string]func(a, b float64) float64{
+		"+": func(a, b float64) float64 { return a + b },
+		"-": func(a, b float64) float64 { return a - b },
+		"*": func(a, b float64) float64 { return a * b },
+		"/": func(a, b float64) float64 { return a / b },
+	}
+	possibleArguments := []string{"int", "float"}
+
+	newLiteralOfType := func(_type string) token.Literal {
+		switch _type {
+		case "int":
+			return token.NewLiteralInt(rand.Int63())
+		case "float":
+			return token.NewLiteralFloat(rand.Float64())
+		}
+		t.Fatal("undefined type " + _type)
+		return token.LiteralNil
+	}
+
+	for opName, op := range operations {
+		for _, aType := range possibleArguments {
+			for _, bType := range possibleArguments {
+				a := newLiteralOfType(aType)
+				b := newLiteralOfType(bType)
+				actual, err := op(a, b)
+				require.NoError(t, err)
+
+				if aType == bType && aType == "int" {
+					require.Equal(t, goldenInts[opName](a.AsInt(), b.AsInt()), actual.AsInt())
+				} else {
+					require.InDelta(t, goldenFloats[opName](a.AsFloat(), b.AsFloat()), actual.AsFloat(), 1e-9)
+				}
+			}
+		}
 	}
 }
-
-func TestAdd_TypeMissmatch(t *testing.T) {
+func TestTypeMissmatch(t *testing.T) {
 	possibleArguments := map[string]token.Literal{
 		"int":    token.NewLiteralInt(1),
 		"float":  token.NewLiteralFloat(1.0),
